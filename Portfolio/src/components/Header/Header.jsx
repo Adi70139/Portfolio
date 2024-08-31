f16cc2f510e3a8604ec3467db8cc89716a6931ad
unknown <vardhan70139@gnail.com>
2024-08-31 19:32:16 +0530

    first commit

---
+++ Portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Nav, Logo, NavLink, Bars, NavMenu, NavBtn } from "./HeaderElements";
+
+const Header = ({ toggle }) => {
+  return (
+    <div className="Container">
+      <Nav>
+        <Logo to="/">
+           <h2 style={{color:'white', fontFamily:'cursive', fontStyle:'italic', fontWeight
+            :'bolder'
+           }}>Aditya..</h2>
+        </Logo>
+        <NavMenu>
+          <NavLink className="menu-item" to="projects" smooth={true}>
+            Projects
+          </NavLink>
+          <NavLink className="menu-item" to="about" smooth={true}>
+            About
+          </NavLink>
+          <NavLink className="menu-item" to="contact" smooth={true}>
+            Contact
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+        <button 
+            onClick={() => window.open('./assets/Aditya_Vardhan_Profile.pdf')} 
+            style={{ padding: '15px', cursor: 'pointer', borderRadius:'20px', border:'1px solid darkblue',
+                fontWeight:'bold'
+            }}
+          >
+            Resume
+          </button>
+        </NavBtn>
+        <Bars onClick={toggle} />
+      </Nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ Portfolio/src/data/ProjectData.js
@@ -0,0 +1,78 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "./assets/FreezerBurns.png",
+    title: "FreezerBurns",
+    description: "Developed a MERN stack application with awesome user interface and core functionalities.",
+    tech_stack: "React, Javascript, Node.js, Express.js, CSS",
+    github_url: "https://github.com/Adi70139/Freezer.com",
+    demo_url: "https://freezer-com.onrender.com",
+  },
+  {
+    img: "./assets/onebox.png",
+    title: "Onebox",
+    description: "An email dashboard where we can view,delete and reply to emails.",
+    tech_stack: "Auth0, React, Javascript, Tailwind CSS",
+    github_url: "https://github.com/Adi70139/OneBox",
+    demo_url: "one-box-reactinbox.vercel.app",
+  },
+  {
+    img: "./assets/SassySpicy.png",
+    title: "SassySpicy",
+    description:
+      "A simple react food ordering app which has filter,search and cart facility.",
+    tech_stack: "React, Javascript, CSS",
+    github_url: "https://github.com/Adi70139/Foody",
+    demo_url: "https://foody-react.netlify.app",
+  },
+  {
+    img: "./assets/portfolio.png",
+    title: "Portfolio",
+    description: "Ofcourse i made my own personal portfolio website.",
+    tech_stack: "Reactjs, CSS",
+    github_url: "https://github.com/Adi70139/Portfolio",
+    demo_url: "https://yet-to-deploy/",
+  },
+  {
+    img: "./assets/Keeper.png",
+    title: "Keeper",
+    description:
+      "Created simple react note app where we add personal tasks.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://keeper-note-react.netlify.app",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+];
